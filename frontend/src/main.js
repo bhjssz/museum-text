f96abe890509a4419adf36cc6207981b91361c0f
Daisy Lee <1986410883@qq.com>
2026-03-12 21:42:57 +0800

    Delete frontend directory

--- frontend/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import RootApp from './RootApp.vue'
-import './index.css'
-
-import router from './router.js'
-
-const app = createApp(RootApp)
-app.use(router)
-app.mount('#app')
